0c284dee2ffafd72578778826c9a576f867805f1
Milon <milon.cs50@yahoo.com>
2019-12-06 14:49:45 +0600

    upload project

--- src/main/java/org/fusion/application/controller/CustomerController.java
+++ src/main/java/org/fusion/application/controller/CustomerController.java
@@ -1,10 +1,14 @@
 package org.fusion.application.controller;
 
+import org.fusion.application.model.Customer;
 import org.fusion.application.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class CustomerController {
@@ -13,8 +17,18 @@ public class CustomerController {
     private CustomerRepository customerRepository;
 
     @RequestMapping("/customer/list")
-    public String getAll(Model model){
-        model.addAttribute("customer", customerRepository.findAll());
+    public String getAll(Model model) {
         return "customer/list";
+    }
+
+    @ModelAttribute("customer")
+    public List<Customer> getAll() {
+        return customerRepository.findAll();
+    }
+
+    @RequestMapping("/customer-save")
+    public String saveNew(Model model) {
+        model.addAttribute("cus", new Customer());
+        return "customer/create_update";
     }
 }
